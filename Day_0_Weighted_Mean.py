c7933d9a2358565ae717a347a0d983ee97219bfb
Matheus Macedo Lopes <matheusmlopess@gmail.com>
2021-06-29 22:44:23 -0300

    addng .py

---
+++ Day_0_Weighted_Mean.py
@@ -0,0 +1,20 @@
+def weightedMean(X, W):
+
+    prod_waited = [ valx * valw for valx, valw in zip(X,W) ]
+    V_sum = sum(prod_waited)
+    W_sum = sum(W)
+
+    mean_waited = V_sum / W_sum
+    #print(V_sum)
+    #print(W_sum)
+    print(mean_waited)
+
+if __name__ == '__main__':
+
+    int(input().strip())                                 #n = 5                                           
+
+    vals = list(map(int, input().rstrip().split()))      #'10 40 30 50 20'
+    
+    weights = list(map(int, input().rstrip().split()))   #'1 2 3 4 5'
+    
+    weightedMean(vals, weights)                          #weightedMean(vals, weights)
