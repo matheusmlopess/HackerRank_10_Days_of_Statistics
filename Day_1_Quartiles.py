9fbf4a5c13f20742d212d169eab7d3a2dd5a5942
Matheus Macedo Lopes <matheusmlopess@gmail.com>
2021-06-30 21:53:48 -0300

    mais um

--- Day_1_Quartiles.py
+++ Day_1_Quartiles.py
@@ -6,7 +6,7 @@ import random
 import re
 import sys
 
-def quartiles2(arr):
+def quartiles(arr):
     
     arr = sorted(arr)
     siz = len(arr)
@@ -23,7 +23,7 @@ def quartiles2(arr):
             Q2 = (arrInf[len(arrInf)// 2] + arrInf[len(arrInf) // 2 - 1 ])/2
             Q3 = (arrSup[len(arrSup)// 2] + arrSup[len(arrSup) // 2 - 1 ])/2
 
-        return [ Q2, Q , Q3]
+        return [ int(Q2), int(Q) , int(Q3)]
 
     else:
         Q = (arr[siz// 2] + arr[siz // 2 - 1 ])/2
@@ -37,7 +37,8 @@ def quartiles2(arr):
             Q2 = (arrInf[len(arrInf)// 2] + arrInf[len(arrInf) // 2 - 1 ])/2
             Q3 = (arrSup[len(arrSup)// 2] + arrSup[len(arrSup) // 2 - 1 ])/2
        
-        return [ Q2, Q , Q3]  
+        return [ int(Q2), int(Q) , int(Q3)]
+
 
 
 if __name__ == '__main__':
@@ -54,13 +55,3 @@ if __name__ == '__main__':
     fptr.write('\n')
 
     fptr.close()
-
-#2
-#19
-#59 60 65 65 68 69 70 72 75 75 76 77 81 82 84 87 90 95 98
-#Quartile 1 (Q1) = 68. Quartile 2 (Q2) = 75 Quartile 2 (Q3) = 84
-
-#3
-#10
-#1 3 3 4 5 6 6 7 8 8
-#Quartile 1 (Q1) = 3. Quartile 2 (Q2) = 5.5.
