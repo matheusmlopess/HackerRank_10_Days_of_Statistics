1e9b7c674f19274f3ba2fcbe81e0cbc59eedfd22
matheusmlopess <matheusmlopess@gmail.com>
2021-07-06 15:34:12 -0300

    final py

--- Day_1_InterquartileRange.py
+++ Day_1_InterquartileRange.py
@@ -6,37 +6,23 @@ import random
 import re
 import sys
 
-#
-# Complete the 'interQuartile' function below.
-#
-# The function accepts following parameters:
-#  1. INTEGER_ARRAY values
-#  2. INTEGER_ARRAY freqs
-#
-
-def interQuartile2(values, freqs):
-    
-    
-    arr = [ values[i] for i,fd in enumerate(freqs, 0) for c in range(fd)]
-    siz = len(arr)
+def quartiles(arr):
     arr = sorted(arr)
-   
+    siz = len(arr)
+    
     if siz%2 >0:
         Q = arr[siz // 2]
         arrInf = arr[:siz // 2]
         arrSup = arr[(siz // 2)+1:]
 
         if (len(arrInf)%2>0):
-            Q1 = arrInf[len(arrInf) // 2]
+            Q2 = arrInf[len(arrInf) // 2]
             Q3 = arrSup[len(arrSup) // 2]
-
         else:
-            Q1 = (arrInf[len(arrInf)// 2] + arrInf[len(arrInf) // 2 - 1 ])/2
+            Q2 = (arrInf[len(arrInf)// 2] + arrInf[len(arrInf) // 2 - 1 ])/2
             Q3 = (arrSup[len(arrSup)// 2] + arrSup[len(arrSup) // 2 - 1 ])/2
 
-        fin = round(float(Q3 - Q1),1)
-        print(fin) 
-        #return [ int(Q1), int(Q) , int(Q3)]
+        return [ int(Q2), int(Q) , int(Q3)]
 
     else:
         Q = (arr[siz// 2] + arr[siz // 2 - 1 ])/2
@@ -44,26 +30,24 @@ def interQuartile2(values, freqs):
         arrSup = arr[(siz // 2):]
 
         if (len(arrInf)%2>0):
-            Q1 = arrInf[len(arrInf) // 2]
+            Q2 = arrInf[len(arrInf) // 2]
             Q3 = arrSup[len(arrSup) // 2]
-
         else:
-            Q1 = (arrInf[len(arrInf)// 2] + arrInf[len(arrInf) // 2 - 1 ])/2
+            Q2 = (arrInf[len(arrInf)// 2] + arrInf[len(arrInf) // 2 - 1 ])/2
             Q3 = (arrSup[len(arrSup)// 2] + arrSup[len(arrSup) // 2 - 1 ])/2
-
-        fin = round(float(Q3 - Q1),1)
-        print(fin) 
-        #return [ int(Q1), int(Q) , int(Q3)]
-
-
+       
+        return [ int(Q2), int(Q) , int(Q3)]
     
 if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
     n = int(input().strip())
 
-    val = list(map(int, input().rstrip().split()))
+    data = list(map(int, input().rstrip().split()))
 
-    freq = list(map(int, input().rstrip().split()))
+    res = quartiles(data)
 
-    interQuartile(val, freq)
-    
-    
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
