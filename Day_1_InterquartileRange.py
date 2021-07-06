81fce7128f21fb63c45bdfc5daa671137ad74050
matheusmlopess <matheusmlopess@gmail.com>
2021-07-06 15:32:45 -0300

    Day 1- chalng II final subission

--- Day_1_InterquartileRange.py
+++ Day_1_InterquartileRange.py
@@ -14,13 +14,13 @@ import sys
 #  2. INTEGER_ARRAY freqs
 #
 
-def interQuartile(values, freqs):
+def interQuartile2(values, freqs):
+    
     
     arr = [ values[i] for i,fd in enumerate(freqs, 0) for c in range(fd)]
-
-    arr = sorted(arr)
     siz = len(arr)
-    
+    arr = sorted(arr)
+   
     if siz%2 >0:
         Q = arr[siz // 2]
         arrInf = arr[:siz // 2]
@@ -29,9 +29,14 @@ def interQuartile(values, freqs):
         if (len(arrInf)%2>0):
             Q1 = arrInf[len(arrInf) // 2]
             Q3 = arrSup[len(arrSup) // 2]
+
         else:
             Q1 = (arrInf[len(arrInf)// 2] + arrInf[len(arrInf) // 2 - 1 ])/2
             Q3 = (arrSup[len(arrSup)// 2] + arrSup[len(arrSup) // 2 - 1 ])/2
+
+        fin = round(float(Q3 - Q1),1)
+        print(fin) 
+        #return [ int(Q1), int(Q) , int(Q3)]
 
     else:
         Q = (arr[siz// 2] + arr[siz // 2 - 1 ])/2
@@ -41,14 +46,14 @@ def interQuartile(values, freqs):
         if (len(arrInf)%2>0):
             Q1 = arrInf[len(arrInf) // 2]
             Q3 = arrSup[len(arrSup) // 2]
+
         else:
             Q1 = (arrInf[len(arrInf)// 2] + arrInf[len(arrInf) // 2 - 1 ])/2
             Q3 = (arrSup[len(arrSup)// 2] + arrSup[len(arrSup) // 2 - 1 ])/2
-       
-        fin = Q3 - Q1
-        print(fin) 
 
-    
+        fin = round(float(Q3 - Q1),1)
+        print(fin) 
+        #return [ int(Q1), int(Q) , int(Q3)]
 
 
     
